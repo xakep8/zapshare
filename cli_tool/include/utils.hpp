37d9f628504ab40a2d9ac2691de1a079e60ab84e
Kunal Dubey <xakep8@protonmail.com>
2025-12-24 00:10:39 +0530

    Refactor message handling and add transfer metadata structure
    - Updated the Messages namespace to Message and added new message types for peer-to-peer protocol.
    - Introduced a new struct `Transfer_Metadata` to encapsulate transfer-related information.
    - Enhanced utility functions to handle JSON parsing for transfer metadata.
    - Implemented file hash computation using SHA-256 in the Crypto namespace.
    - Developed a basic client implementation to connect to a TCP server and send data.
    - Improved error handling and command validation in the main function.

--- cli_tool/include/utils.hpp
+++ cli_tool/include/utils.hpp
@@ -1,7 +1,19 @@
 #pragma once
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+#include <openssl/sha.h>
 
 #include <filesystem>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <string>
+
+#include "httplib.h"
+#include "json.hpp"
+#include "types.h"
+
+using json = nlohmann::json;
 
 namespace Error {
 void print_usage() { std::cerr << "usage:\n" << "    zapshare send [filepath]\n" << "    zapshare get [secret]\n"; }
@@ -18,10 +30,77 @@ void invalid_file_path() {
 }  // namespace Error
 
 namespace Utils {
-bool check_file_exists(const std::string_view filepath) {
+inline TRANSFERS transfer_metadata_from_json(const json& data) {
+    try {
+        TRANSFERS t;
+        t.id = data["id"];
+        t.sender_ip = data["sender_ip"];
+        t.sender_port = data["sender_port"];
+        t.protocol = data["protocol"];
+        t.file_name = data["file_name"];
+        t.file_size = data["file_size"];
+        t.file_hash = data["file_hash"];
+        t.token = data["token"];
+        return t;
+    } catch (std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << "\n";
+        throw std::runtime_error(e.what());
+    }
+}
+
+bool check_file_exists(const std::string_view& filepath) {
     std::filesystem::path file{filepath};
     return std::filesystem::exists(file);
 }
 
-bool look_up(const std::string_view secret) { return false; }
+bool look_up(const std::string_view secret) {
+    httplib::Client client("http://127.0.0.1:3000");
+    const std::string url = "/lookup/" + std::string(secret);
+    if (auto res = client.Get(url)) {
+        if (res->body == "True") {
+            return true;
+        }
+    }
+    return false;
+}
+
+TRANSFERS get_transfer_metadata(const std::string_view& secret) {
+    httplib::Client client("http://127.0.0.1:3000");
+    const std::string url = "/getfile/" + std::string(secret);
+    if (auto res = client.Get(url)) {
+        return transfer_metadata_from_json(json::parse(res->body));
+    }
+    throw std::runtime_error("[GET TRANSFER METADATA] Error getting metadata for the transfer");
+}
 }  // namespace Utils
+
+namespace Crypto {
+std::string compute_file_hash(const std::string_view& path) {
+    std::ifstream file(std::string(path), std::ifstream::binary);
+    if (!file.is_open()) {
+        throw std::runtime_error("[CRYPTO] Error opening file: ");
+    }
+
+    SHA256_CTX sha256_context;
+    SHA256_Init(&sha256_context);
+    const int buffer_size = 4096;
+    unsigned char buffer[buffer_size];
+    while (file.read(reinterpret_cast<char*>(buffer), buffer_size)) {
+        SHA256_Update(&sha256_context, buffer, file.gcount());
+    }
+
+    if (file.gcount() > 0) {
+        SHA256_Update(&sha256_context, buffer, file.gcount());
+    }
+
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_Final(hash, &sha256_context);
+    file.close();
+
+    std::stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
+    }
+    return ss.str();
+}
+}  // namespace Crypto
--- cli_tool/main.cpp
+++ cli_tool/main.cpp
@@ -10,31 +10,62 @@ void start_server() {
     io.run();
 }
 
-void start_client() {}
+void start_client() {
+    using asio::ip::tcp;
+    asio::io_context io_context;
+
+    // we need a socket and a resolver
+    tcp::socket socket(io_context);
+    tcp::resolver resolver(io_context);
+
+    // now we can use connect(..)
+    asio::connect(socket, resolver.resolve("127.0.0.1", "25000"));
+    while (true) {
+        std::string data{"some client data ..."};
+        auto result = asio::write(socket, asio::buffer(data));
+
+        // the result represents the size of the sent data
+        std::cout << "data sent: " << data.length() << '/' << result << std::endl;
+    }
+
+    // and close the connection now
+    asio::error_code ec;
+    socket.shutdown(asio::ip::tcp::socket::shutdown_both, ec);
+    socket.close();
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         Error::print_usage();
+        return 1;
     }
     const std::string_view cmd = argv[1];
     if (cmd == Command::SEND) {
         if (argc < 3) {
             Error::invalid_file_path();
+            return 1;
         }
         const std::string_view filepath = argv[2];
         if (!Utils::check_file_exists(filepath)) {
             Error::invalid_file_path();
+            return 1;
         }
     } else if (cmd == Command::GET) {
         if (argc < 3) {
             Error::invalid_secret();
+            return 1;
         }
         const std::string_view secret = argv[2];
         if (!Utils::look_up(secret)) {
+            Error::invalid_file_path();
+            return 1;
         }
+        Transfer_Metadata data = Utils::get_transfer_metadata(secret);
+        start_client();
     } else {
         std::cerr << "Invalid Command!\n";
         Error::print_usage();
+        return 1;
     }
     return 0;
 }
